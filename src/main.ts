40e8c498db76d17e649cb36eebd0d80fc0bc5c22
Simon Alberto Lopez Higuera <lopezhsimon@gmail.com>
2022-07-19 15:17:17 -0500

    vueI18n package configured

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,22 @@
 import { createApp } from 'vue'
+import { createI18n } from 'vue-i18n'
 import { createPinia } from 'pinia'
 import App from './App.vue'
 import './assets/css/main.css'
+
+import es from './locales/es.json'
+
+const i18n = createI18n({
+    legacy: false,
+    locale: 'es',
+    messages: {
+      es
+    }
+})
 
 const pinia = createPinia()
 const app = createApp(App)
 
 app.use(pinia)
+app.use(i18n)
 app.mount('#app')
