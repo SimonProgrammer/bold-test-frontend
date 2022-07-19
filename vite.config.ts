32b2ec4f08e78e164dbd347124751349dc2067aa
Simon Alberto Lopez Higuera <lopezhsimon@gmail.com>
2022-07-19 14:24:09 -0500

    sass global variables configured

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,20 @@
+import path from 'path'
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue()]
+  plugins: [vue()],
+  css: {
+    preprocessorOptions: {
+      scss: {
+        additionalData: `@import "@/config/variables.scss";`
+      }
+    }
+  },
+  resolve: {
+    alias: {
+      '@': path.resolve(__dirname, 'src'),
+    }
+  }
 })
